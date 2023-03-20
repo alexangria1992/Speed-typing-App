64c069a373e3dde55d86184e440725624407ee86
alexangria1992 <alex040892@gmail.com>
2023-03-20 14:07:14 +1300

    building countdown timer

--- speed-typing/src/App.tsx
+++ speed-typing/src/App.tsx
@@ -4,10 +4,19 @@ import { faker } from "@faker-js/faker";
 const words = faker.random.words(10);
 
 const App = () => {
-  return <GenerateWords words={words} />;
+  return (
+    <>
+      <CountdownTimer timeLeft={30} />
+      <GenerateWords words={words} />;
+    </>
+  );
 };
 
 const GenerateWords = ({ words }: { words: string }) => {
   return <div className="text-4xl text-center text-slate-500">{words}</div>;
+};
+
+const CountdownTimer = ({ timeLeft }: { timeLeft: number }) => {
+  return <h2 className="text-primary-500 font-medium">Time: {timeLeft}</h2>;
 };
 export default App;
