76c219fa88b606dff41e23b764f1260fc68e61c1
alexangria1992 <alex040892@gmail.com>
2023-03-20 14:04:20 +1300

    Generating Random Words

--- speed-typing/src/App.tsx
+++ speed-typing/src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { faker } from "@faker-js/faker";
 
-function App() {
-  return <div className="text-4xl text-center">It works</div>;
-}
+const words = faker.random.words(10);
 
+const App = () => {
+  return <GenerateWords words={words} />;
+};
+
+const GenerateWords = ({ words }: { words: string }) => {
+  return <div className="text-4xl text-center text-slate-500">{words}</div>;
+};
 export default App;
